a845c2ae511af883a1f154900807d46514f732d0
adrianaf <adrianaf@DESKTOP-BLMD6U1>
2022-10-15 13:49:28 +0400

    Add project files.

---
+++ Q2Ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿// 2. დაწერეთ პროგრამა რომელიც სტრინგისთვის დაბეჭდავს არის თუ არა მოცემული სტრინგი პალინდრომი
+// (პალინდრომის მეთოდი გაიტანეთ ცალკე და გამოიძახეთ სხვადასხვა სტრინგისთვის) (2)
+
+
+
+int i = 0;
+int j = value.Length - 1;
+bool IsPalindrome(string value)
+while (true)
+{
+    if (i > j)
+    {
+        return true;
+    }
+    char a = value[i];
+    char b = value[j];
+    if (char.ToLower(a) != char.ToLower(b))
+    {
+        return false;
+    }
+    i++;
+    j--;
+}
---
+++ Q2Ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿// 4. (ბონუსი) დაწერეთ პროგრამა რომელიც მასივში იპოვნის ყველაზე ხშირად გამეორებული ელემენტის რაოენობას და დაბეჭდავს ამ ელემენტს. (2)
+
+int a(int[] a)
+{
+    int[] arr = a;
+    int c = 1, maxcount = 1, maxvalue = 0;
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        maxvalue = arr[i];
+        for (int j = 0; j < arr.Length; j++)
+        {
+            if (maxvalue == arr[j] && j != i)
+            {
+                c++;
+                if (c > maxcount)
+                {
+                    maxcount = c;
+                    result = arr[i];
+                }
+            }
+            else
+            {
+                c = 1;
+            }
+        }
+    }
