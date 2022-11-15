9a1cdab440de85aadc848ca4a35dec974d146d1c
natamamia <natamamia@gmail.com>
2022-11-15 17:18:58 +0400

    Add files via upload

--- Q2Ex4/Program.cs
+++ Q2Ex4/Program.cs
@@ -1,27 +1,12 @@
 ﻿// 4. (ბონუსი) დაწერეთ პროგრამა რომელიც მასივში იპოვნის ყველაზე ხშირად გამეორებული ელემენტის რაოენობას და დაბეჭდავს ამ ელემენტს. (2)
 
-int a(int[] a)
-{
-    int[] arr = a;
-    int c = 1, maxcount = 1, maxvalue = 0;
-    int result = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        maxvalue = arr[i];
-        for (int j = 0; j < arr.Length; j++)
-        {
-            if (maxvalue == arr[j] && j != i)
-            {
-                c++;
-                if (c > maxcount)
-                {
-                    maxcount = c;
-                    result = arr[i];
-                }
-            }
-            else
-            {
-                c = 1;
-            }
-        }
-    }
+
+Console.WriteLine("Duplicate number");
+int[] number = new int[10] { 5, 5, 9, 6, 5, 5, 5, 8, 3, 7 };
+
+var duplicates = number.GroupBy(x => x)
+              .Where(g => g.Count() > 1)
+              .Select(y => y.Key)
+              .ToList();
+
+Console.WriteLine(String.Join(", ", duplicates));
